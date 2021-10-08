95a2567a0cf6629a196f1846a0daf002daf88e6d
Paulo-Mason-SkyDark <paulodanielaraujo.pd90@gmail.com>
2021-10-07 23:35:45 -0400

    feat(adding structu for task of view task)

---
+++ front-end/src/components/visualizar_tarefa/style.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div `
+    //TODO: margin lateral e do topo temporario, remover
+    margin: 30px 30px;
+    padding: 1.5rem 2.0625rem;
+    width: 41.875rem;
+    height: 31.125rem;
+    background-color: grey;
+    border-radius: 0.75rem;
+
+`;
+
+export const CardDescricao = styled.div`
+
+    width: 17.75rem;
+    height: 28.3125rem;
+    background-color: white;
+
+
+`;
---
+++ front-end/src/components/visualizar_tarefa/visualizar_tarefa.tsx
@@ -0,0 +1,12 @@
+//import styles
+import { Container, CardDescricao } from "./style";
+
+export function Teste(){
+    return(
+        <Container>
+            <CardDescricao>
+                
+            </CardDescricao>
+        </Container>
+    );
+}
