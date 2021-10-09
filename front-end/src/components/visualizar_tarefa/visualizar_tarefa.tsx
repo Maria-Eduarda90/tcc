a245147dd8591ca561826f16f2bc0b8504aaca39
Paulo-Mason-SkyDark <paulodanielaraujo.pd90@gmail.com>
2021-10-08 23:14:15 -0400

    feat(visualizar_tarefa): adding title of task, paragraph and footer img

--- front-end/src/components/visualizar_tarefa/visualizar_tarefa.tsx
+++ front-end/src/components/visualizar_tarefa/visualizar_tarefa.tsx
@@ -1,11 +1,22 @@
 //import styles
-import { Container, CardDescricao } from "./style";
+import { Container, CardDescricao, TituloTarefa, DescricaoTarefa, FooterTarefa } from "./style";
 
-export function Teste(){
-    return(
+export function Teste() {
+    return (
         <Container>
             <CardDescricao>
-                
+                <TituloTarefa>Fazer um orçamento</TituloTarefa>
+                <DescricaoTarefa>Praesent a iaculis lorem. Duis varius varius aliquam.
+                    Maecenas lacinia cursus facilisis. Aliquam egestas scelerisque sapien.
+                    Praesent a iaculis lorem. Duis varius varius aliquam. Maecenas lacinia
+                    cursus facilisis. Aliquam egestas scelerisque sapien.</DescricaoTarefa>
+                <FooterTarefa>
+                    <div className="user">
+                        <div className="image">
+                            <img src="https://i.pinimg.com/originals/43/17/19/431719fbf11680dda780e19cfb40b013.jpg" alt="profile" />
+                        </div>
+                    </div>
+                </FooterTarefa>
             </CardDescricao>
         </Container>
     );
