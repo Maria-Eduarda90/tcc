0b51b313de39ce2b9e5bfb85ffa416c68adef6ef
Paulo-Mason-SkyDark <paulodanielaraujo.pd90@gmail.com>
2021-10-08 23:12:22 -0400

    feat(styles, visualizar): adding update in style component visualizar tarefa img, paragrafo titulo

--- front-end/src/components/visualizar_tarefa/style.ts
+++ front-end/src/components/visualizar_tarefa/style.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Container = styled.div `
+export const Container = styled.div`
     //TODO: margin lateral e do topo temporario, remover
     margin: 30px 30px;
     padding: 1.5rem 2.0625rem;
@@ -17,5 +17,42 @@ export const CardDescricao = styled.div`
     height: 28.3125rem;
     background-color: white;
 
+
+`;
+
+export const TituloTarefa = styled.h3`
+
+    color: var(--color-card);
+    font-family: "Lato", sans-serif;
+    text-align: center;
+    height: 1.1875rem;
+    font-size: 1rem;
+    line-height: 19px;
+
+`;
+
+export const DescricaoTarefa = styled.p`
+
+    width: 236px;
+    height: 195px;
+    text-align: left;
+    color: var(--grey-90);
+    margin: 1.5rem 1.5rem 0 1.5rem;
+    font-family: Lato, sans-serif;
+    font-size: 12px;
+    line-height: 22px;
+
+`;
+
+
+export const FooterTarefa = styled.div`
+
+    margin-top: 192px;
+
+    
+    .user .image{
+        width: 2rem;
+        height: 2rem;
+    }
 
 `;
