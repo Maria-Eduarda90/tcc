d4318d4848a4ae9daed4a7fe0552a662090ce0de
Paulo-Mason-SkyDark <paulodanielaraujo.pd90@gmail.com>
2021-10-09 22:17:02 -0400

    I styled the footer with the user's image and priority

--- front-end/src/components/visualizar_tarefa/style.ts
+++ front-end/src/components/visualizar_tarefa/style.ts
@@ -3,19 +3,23 @@ import styled from 'styled-components';
 export const Container = styled.div`
     //TODO: margin lateral e do topo temporario, remover
     margin: 30px 30px;
+    display: flex;
     padding: 1.5rem 2.0625rem;
-    width: 41.875rem;
+    width: 43.75rem;
     height: 31.125rem;
     background-color: grey;
     border-radius: 0.75rem;
 
 `;
 
-export const CardDescricao = styled.div`
+export const CardTarefa = styled.div`
 
+    position: relative;
     width: 17.75rem;
     height: 28.3125rem;
     background-color: white;
+    margin-right: 2rem;
+
 
 
 `;
@@ -47,12 +51,45 @@ export const DescricaoTarefa = styled.p`
 
 export const FooterTarefa = styled.div`
 
-    margin-top: 192px;
-
+    /* margin-top: 10rem; */
+    position: absolute;
+    bottom: .3125rem;
+    margin-left: 0.375rem;
+    width: 17.75rem;
     
-    .user .image{
+    .flex-container {
+        display: flex;
+        align-items: center;
+    }
+
+    .flex-container p{
+        font-family: Poppins;
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 21px;
+        color: var(--grey-90);
+        span{
+            margin-left: .75rem;
+        }
+    }
+    
+    img{
         width: 2rem;
         height: 2rem;
+        border-radius: 1rem;
+        margin: 0;
+        margin-right: 9.125rem;
     }
+
+`;
+
+export const Divisor = styled.hr`
+
+    color: #C4C4C4;
+    width: 1px;
+    height: 453px;
+
+    //TODO: trocar pixel por rem
+
 
 `;
