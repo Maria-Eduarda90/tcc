f4217ce2426c2f97763747416f206bffc9f5cb44
Paulo-Mason-SkyDark <paulodanielaraujo.pd90@gmail.com>
2021-10-09 22:19:41 -0400

    I created footer user and improved some things in the html to better style. I created a splitter using an hr to split the screen

--- front-end/src/components/visualizar_tarefa/visualizar_tarefa.tsx
+++ front-end/src/components/visualizar_tarefa/visualizar_tarefa.tsx
@@ -1,23 +1,24 @@
 //import styles
-import { Container, CardDescricao, TituloTarefa, DescricaoTarefa, FooterTarefa } from "./style";
+import { Container, CardTarefa, TituloTarefa, DescricaoTarefa, FooterTarefa, Divisor } from "./style";
 
 export function Teste() {
     return (
         <Container>
-            <CardDescricao>
+            <CardTarefa>
                 <TituloTarefa>Fazer um orçamento</TituloTarefa>
                 <DescricaoTarefa>Praesent a iaculis lorem. Duis varius varius aliquam.
                     Maecenas lacinia cursus facilisis. Aliquam egestas scelerisque sapien.
                     Praesent a iaculis lorem. Duis varius varius aliquam. Maecenas lacinia
-                    cursus facilisis. Aliquam egestas scelerisque sapien.</DescricaoTarefa>
+                    cursus facilisis. Aliquam egestas scelerisque sapien.
+                </DescricaoTarefa>
                 <FooterTarefa>
-                    <div className="user">
-                        <div className="image">
-                            <img src="https://i.pinimg.com/originals/43/17/19/431719fbf11680dda780e19cfb40b013.jpg" alt="profile" />
-                        </div>
+                    <div className="flex-container">
+                        <img src="https://i.pinimg.com/originals/43/17/19/431719fbf11680dda780e19cfb40b013.jpg" alt="profile" />
+                        <p>Prioridade:<span>1</span></p>
                     </div>
                 </FooterTarefa>
-            </CardDescricao>
+            </CardTarefa>
+            <Divisor/>
         </Container>
     );
 }
