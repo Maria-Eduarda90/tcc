d2e9390111cf5d470f8ffd0c7015e9af7deadf3d
Paulo-Mason-SkyDark <paulodanielaraujo.pd90@gmail.com>
2021-10-18 18:26:58 -0400

    feat(style): adding added card styling and its functionality

--- front-end/src/components/visualizar_tarefa/style.ts
+++ front-end/src/components/visualizar_tarefa/style.ts
@@ -9,6 +9,21 @@ export const Container = styled.div`
     height: 31.125rem;
     background-color: grey;
     border-radius: 0.75rem;
+    position: relative;
+
+    .menu-barsModal{
+        color: #C4C4C4;
+        display: flex;
+        position: absolute;
+        right: 0.75rem;
+        top: 0.75rem;
+        width: 1.5rem;
+        height: 1.5rem;
+        font-size: 1.2rem;
+        
+    }
+
+
 
 `;
 
@@ -63,7 +78,7 @@ export const FooterTarefa = styled.div`
     }
 
     .flex-container p{
-        font-family: Poppins;
+        font-family: Poppins, sans-serif;
         font-weight: 500;
         font-size: 14px;
         line-height: 21px;
@@ -86,10 +101,75 @@ export const FooterTarefa = styled.div`
 export const Divisor = styled.hr`
 
     color: #C4C4C4;
-    width: 1px;
-    height: 453px;
+    width: 0.0625rem;
+    height: 28.3125rem;
 
     //TODO: trocar pixel por rem
 
 
+`;
+
+export const CommentBoxForm = styled.form`
+
+    margin-top: 1.0625rem;
+    margin-left: 0.4375rem;
+
+     > h4 {
+
+        width: 7.5625rem;
+        height: 1.0625rem;
+        font-family: Poppins;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 11px;
+        line-height: 16px;
+        color: var(--color-card);
+    }
+
+    label{
+        cursor: pointer;
+        display: flex;
+        justify-content: flex-end;
+        font-family: Poppins, sans-serif;
+        font-size: 0.687rem;
+        line-height: 1rem;
+        // trocar para a cor grey-90
+        color: white;
+    }
+
+    input[type='file'] {
+        border: none;
+        display: none;
+    }
+
+    
+`;
+
+export const TextArea = styled.textarea`
+    width: 19.5rem;
+    height: 3.187rem;
+    outline: none;
+    resize: none;
+    padding: 1rem;
+    border-radius: 0.5rem;
+    /* border: 1px solid var(--grey-100); */
+    border: 0.0625rem solid red;
+`;
+
+export const CardBoxRender = styled.div`
+    position: absolute;
+    right: 1.5rem;
+    bottom: 1.5rem;
+    display: flex;
+    flex-direction: column;
+
+`;
+
+
+export const Card = styled.div`
+    width: 19.5rem;
+    height: 3.187rem;
+    border-radius: 0.5rem;
+    /* border: 1px solid var(--grey-100); */
+    border: 0.0625rem solid red;
 `;
