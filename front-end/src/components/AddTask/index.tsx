12c2393e316e9e9089a45a976a2d94e2486c16bf
Mary Skywalker <snowdennaruto90@gmail.com>
2021-11-01 00:12:41 -0400

    wip

--- front-end/src/components/AddTask/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as C from './style';
-
-export const AddTask = () => {
-    return(
-        <C.Container>
-            <C.Input type="text" placeholder="Adicionar Tarefa"/>
-        </C.Container>
-    );
-}
--- front-end/src/pages/kanbanBoard.tsx
+++ front-end/src/pages/kanbanBoard.tsx
@@ -1,7 +1,3 @@
-import React, { useState } from 'react'
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-
 import Sidebarkanbanboard from '../components/parts/SidebarKanbanboard';
 
 import usePersistedState from '../utils/usePersistedState';
@@ -15,29 +11,20 @@ import dark from '../styles/themes/dark';
 
 import { Container } from '../styles/components/parts/SidebarKanbanboard';
 
-import { Board } from '../components/Board';
 
 export function KanbanBoard() {
     const [ theme, setTheme ] = usePersistedState('theme', light);
-    const [ index, setIndex ] = useState(1);
 
     const toggleTheme = () => {
         setTheme(theme.title === 'light' ? dark : light);
     }
 
-    function moveCard() {
-        setIndex(index)
-    }
-
     return(
-        <DndProvider backend={HTML5Backend}>
             <ThemeProvider theme={theme}>
                 <Container>
                     <GlobalStyle/>
                     <Sidebarkanbanboard toggleTheme={toggleTheme}/>
-                    <Board card={index === 1} moveCard={moveCard.bind(null, 1)}/>
                 </Container>
             </ThemeProvider>
-        </DndProvider>
     );
 }
