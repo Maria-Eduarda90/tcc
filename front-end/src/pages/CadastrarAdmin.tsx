5151c64dda8cd524756b24b9583adac651a64720
Paulo Daniel <paulo.araujo@uzzipay.com>
2021-11-01 00:31:33 -0300

    feat(CadastrarAdmin.tsx):removed import reacts unnecessary;

--- front-end/src/pages/CadastrarAdmin.tsx
+++ front-end/src/pages/CadastrarAdmin.tsx
@@ -1,21 +1,25 @@
-import React,{ useState } from 'react';
+import { useState } from 'react';
 import background from '../images/background.png';
 import { Link } from 'react-router-dom';
 
 import '../styles/pages/cadastrarAdmin.css';
 
-export function CadastrarAdmin(){
+export function CadastrarAdmin() {
     const [name, setName] = useState('');
     const [nameEmpresa, setNameEmpresa] = useState('');
 
-    const handlerInput = () =>{
-        if(name.length >= 5 && nameEmpresa.length >= 5 ){
+    const handlerInput = () => {
+        if (name.length >= 5 && nameEmpresa.length >= 5) {
             return true;
         }
     }
+    const validateInput = handlerInput();
 
-    var validateInput = handlerInput();
-    return(
+    const testeOnclickDisabled = () => {
+        console.log("entrou");
+    }
+
+    return (
         <div className="container">
             <div className="img">
                 <img src={background} alt="fundo" />
@@ -25,28 +29,30 @@ export function CadastrarAdmin(){
                     <div className="div-h1">
                         <h1 className="h1">Cadastramento</h1>
                     </div>
-                    
+
                     <div className="content">
                         <div className="input-div">
                             <div>
                                 <h1>Seu nome</h1>
                                 <label className="sr-only" htmlFor="text">seu nome</label>
-                                <input type="text" name="text" id="text" onChange={e => setName(e.target.value)} required/>
+                                <input type="text" name="text" id="text" onChange={e => setName(e.target.value)} required />
                             </div>
                         </div>
                         <div className="input-icons">
                             <div>
                                 <h1>Nome da sua empresa</h1>
                                 <label className="sr-only" htmlFor="text">Nome da empresa</label>
-                                <input type="text" name="text" id="text" onChange={e => setNameEmpresa(e.target.value)} required/>
+                                <input type="text" name="text" id="text" onChange={e => setNameEmpresa(e.target.value)} required />
                             </div>
                         </div>
                     </div>
 
                     <div className="button">
-                            <button className={validateInput ? "continueCorazul" : "continue"} disabled = {!validateInput}>
+                        <Link to="createToken" className="LinkPropsButton register">
+                            <button className={validateInput ? "continueCorazul" : "continue"} disabled={!validateInput} onMouseOut={testeOnclickDisabled}>
                                 CONTINUAR
                             </button>
+                        </Link>
                     </div>
                 </form>
             </div>
