4ab4ef9ddb9ef0c9643aa177c5ece0245feb0bf8
salazar00040 <paulocesarmeurerr@hotmail.com>
2021-11-01 21:13:29 -0300

    removendo formik

--- front-end/src/pages/CadastrarAdmin.tsx
+++ front-end/src/pages/CadastrarAdmin.tsx
@@ -2,12 +2,8 @@ import { useState } from 'react';
 import background from '../images/background.png';
 import { Link } from 'react-router-dom';
 
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
 
 import '../styles/pages/cadastrarAdmin.css';
-import { validations } from '../validation/validation';
-import { ErrorMessage } from '../styles/validationStyled/validationStyled';
 
 export function CadastrarAdmin() {
     const [name, setName] = useState('');
@@ -20,23 +16,16 @@ export function CadastrarAdmin() {
     }
     const validateInput = handlerInput();
 
-    const formik = useFormik({
-        initialValues: { nome: '', nome_da_empresa: '' },
-        validationSchema: Yup.object(validations),
-        onSubmit: (values) => {},
-    });
+    
 
-    const testeOnclickDisabled = () => {
-        console.log("entrou");
-    }
-
+    
     return (
         <div className="container">
             <div className="img">
                 <img src={background} alt="fundo" />
             </div>
             <div className="container-form">
-                <form className="form" action="" method="POST" onSubmit={formik.handleSubmit}>
+                <form className="form" action="" method="POST">
                     <div className="div-h1">
                         <h1 className="h1">Cadastramento</h1>
                     </div>
@@ -47,15 +36,8 @@ export function CadastrarAdmin() {
                                 <h1>Seu nome</h1>
                                 <label className="sr-only" htmlFor="text">seu nome</label>
                                 <input type="text" name="text" id="text" onChange={e => setName(e.target.value)} required />
-                                <input
-                                 type="text" 
-                                 name="nome"
-                                 id="nome"
-                                 onChange={formik.handleChange}
-                                 value={formik.values.nome}
-                                 />
+                                
                             </div>
-                            {formik.errors.nome && formik.touched.nome && <ErrorMessage>{formik.errors.nome}</ErrorMessage>}
                         </div>
                         <div className="input-icons">
                             <div>
@@ -63,18 +45,14 @@ export function CadastrarAdmin() {
                                 <label className="sr-only" htmlFor="text">Nome da empresa</label>
                                 <input type="text" name="text" id="text" onChange={e => setNameEmpresa(e.target.value)} required />
                                 <label className="sr-only" htmlFor="nome_da_empresa">Nome da empresa</label>
-                                <input type="text" name="nome_da_empresa" id="nome_da_empresa"
-                                 onChange={formik.handleChange}
-                                 value={formik.values.nome_da_empresa}
-                                />
+                               
                             </div>
-                            {formik.errors.nome_da_empresa && formik.touched.nome_da_empresa && <ErrorMessage>{formik.errors.nome_da_empresa}</ErrorMessage>}
                         </div>
                     </div>
 
                     <div className="button">
                         <Link to="createToken" className="LinkPropsButton register">
-                            <button className={validateInput ? "continueCorazul" : "continue"} disabled={!validateInput} onMouseOut={testeOnclickDisabled}>
+                            <button className={validateInput ? "continueCorazul" : "continue"} disabled={!validateInput}>
                                 CONTINUAR
                             </button>
                         </Link>
--- front-end/src/pages/CreateToken.tsx
+++ front-end/src/pages/CreateToken.tsx
@@ -2,10 +2,7 @@ import { useState } from 'react';
 import background from '../images/background.png';
 import '../styles/pages/createToken.css';
 
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
 
-import { validations } from '../validation/validation';
 
 import { ErrorMessage } from '../styles/validationStyled/validationStyled';
 
@@ -25,11 +22,7 @@ export function CreateToken() {
     }
 
     var validateInput = handlerInput();
-    const formik = useFormik({
-        initialValues: { email: '', senha: '', confirmacao: '' },
-        validationSchema: Yup.object(validations),
-        onSubmit: (values) => {},
-    });
+   
 
     return (
         <div className="container">
@@ -37,36 +30,23 @@ export function CreateToken() {
                 <img src={background} alt="fundo" />
             </div>
             <div className="container-form">
-            <form  className="form-token" action="" method="POST" onSubmit={formik.handleSubmit}>
+            <form  className="form-token" action="" method="POST" >
                 <div className="input-div">
                     <div className="div-icon">
                         <h1>Seu email</h1>
                         <label className="sr-only" htmlFor="email">email</label>
                         <input type="email" name="email" id="email" required onChange={e => setEmail(e.target.value)}/>
-                        <input
-                         type="text" 
-                         name="email" 
-                         id="email"
-                         onChange={formik.handleChange}
-                         value={formik.values.email}
-                         />
+                       
                          
                     </div>
-                    {formik.errors.email && formik.touched.email && <ErrorMessage>{formik.errors.email}</ErrorMessage>}
                 </div>
                 <div className="input-icons">
                     <div className="div-iconTwo">
                         <h1>Senha</h1>
                         <label className="sr-only" htmlFor="password">senha</label>
                         <input type="password" name="password" id="password" required onChange={e => setSenha(e.target.value)}/>
-                        <input
-                         type="password" 
-                         name="senha"
-                         id="senha" 
-                         onChange={formik.handleChange}
-                         value={formik.values.senha}/>
+                        
                     </div>
-                    {formik.errors.senha && formik.touched.senha && <ErrorMessage>{formik.errors.senha}</ErrorMessage>}
                 </div>
                 <div className="input-icons">
                     <div className="div-iconTwo">
@@ -74,14 +54,8 @@ export function CreateToken() {
                         <label className="sr-only" htmlFor="password">confirmSenha</label>
                         <input type="password" name="password" id="password" required onChange={e => setConfirmSenha(e.target.value)} />
                         <label className="sr-only" htmlFor="password">senha</label>
-                        <input
-                         type="password" 
-                         name="confirmacao" 
-                         id="confirmacao" 
-                         onChange={formik.handleChange}
-                         value={formik.values.confirmacao}/>
+                       
                     </div>
-                    {formik.errors.confirmacao && formik.touched.confirmacao && <ErrorMessage>{formik.errors.confirmacao}</ErrorMessage>}
                 </div>
                 <div className="token">
                     <div>
