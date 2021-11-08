e6010cc7da6a0bf4ec17191115580739e9e6f475
Paulo Daniel <paulo.araujo@uzzipay.com>
2021-11-08 01:15:44 -0300

    merge

--- back-end/src/controller/CollaboratorController.ts
+++ back-end/src/controller/CollaboratorController.ts
@@ -1,10 +1,39 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { Collaborator } from '../models/Collaborator';
 
 class CollaboratorController {
     async create(request: Request, response: Response) {
-        const body = request.body;
-        console.log(body);
-        return response.send();
+        const {
+            name,
+            sobrenome,
+            email,
+            password,
+            // administrador_id,
+            // tarefas_id
+        } = request.body;
+
+        const collaboratorRepository = getRepository(Collaborator);
+
+        const requestImages = request.files as Express.Multer.File[];
+
+        const images = requestImages.map(image => {
+            return {path: image.filename}
+        })
+
+        const collaborator = collaboratorRepository.create({
+            name,
+            sobrenome,
+            email,
+            password,
+            images
+            // administrador_id,
+            // tarefas_id
+        })
+
+        await collaboratorRepository.save(collaborator);
+
+        return response.status(201).json(collaborator)
     }
 }
 
--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -1,10 +1,24 @@
 import { Router } from 'express';
+import multer from 'multer'
+
+import uploadConfig from './config/upload';
 import { CollaboratorController } from './controller/CollaboratorController';
+import { AdministradorController } from './controller/AdministradorController';
+import { TarefasController } from './controller/TarefasController';
 
 const router = Router();
+const upload = multer(uploadConfig);
 
+const administradorController = new AdministradorController;
 const collaboratorController = new CollaboratorController;
+const tarefasController = new TarefasController;
 
-router.post("/collaborator", collaboratorController.create);
+router.post("/collaborator", upload.array('images'), collaboratorController.create);
+router.get("/collaborator/:id", administradorController.show);
+
+router.get("/administrador", administradorController.index);
+router.post("/administrador", upload.array('images'), administradorController.create);
+
+router.post("/tarefas", tarefasController.create);
 
 export { router };
