3b3ccde8da426622d1ff8d5d19f98147d919e583
Mary Skywalker <snowdennaruto90@gmail.com>
2021-11-17 08:23:20 -0400

    trocando o campo sobrenome por senha

--- front-end/src/components/parts/ModalCollaborator.tsx
+++ front-end/src/components/parts/ModalCollaborator.tsx
@@ -87,8 +87,8 @@ const ModalCollaborator: React.FC = () => {
                             <p>Nome</p>
                             <input type="text" name="text" id="text" required/>
 
-                            <p>Sobrenome</p>
-                            <input type="text" name="text" id="text" required/>
+                            <p>Senha</p>
+                            <input type="password" name="password" id="password" required/>
 
                             <p>Email</p>
                             <input type="email" name="email" id="email" required/>
--- front-end/src/services/api.ts
+++ front-end/src/services/api.ts
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-import axios from 'axios'
-
-const api = axios.create({
-    baseURL: 'http://localhost:3333',
-})
-=======
 import axios from 'axios';
 
 const api = axios.create({
     baseURL: 'http://localhost:3333',
-});
->>>>>>> b84f0f1ed1312b597ebfc9f5d9390feb2991d489
-
+})
 export default api;
