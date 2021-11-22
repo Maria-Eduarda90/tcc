f8cd30f891678029cb02d2cd0140a0ef54513743
Mary Skywalker <snowdennaruto90@gmail.com>
2021-11-22 16:58:49 -0400

    wip resolved merge

--- front-end/src/pages/CadastrarAdmin.tsx
+++ front-end/src/pages/CadastrarAdmin.tsx
@@ -56,31 +56,6 @@ export function CadastrarAdmin() {
                             {validateNameEmpresa ? null : <p><ErrorMessage>O nome da empresa precisa ter no mínimo 5 characters</ErrorMessage></p>}
 
                         </div>
-                        <div className="div-h1">
-                            <h1 className="h1">Cadastramento</h1>
-                        </div>
-                        <div className="content">
-                            <div className="input-div">
-                                <div>
-                                    <h1>Seu nome</h1>
-                                    <label className="sr-only" htmlFor="text">seu nome</label>
-                                    <input type="text" name="text" id="text" onChange={e => setName(e.target.value)} required />
-
-                                    {validateName ? null : <p><span>O seu nome precisa ter no mínimo 5 characters</span></p>}
-                                </div>
-                            </div>
-                            <div className="input-icons">
-                                <div>
-                                    <h1>Nome da sua empresa</h1>
-                                    <label className="sr-only" htmlFor="text">Nome da empresa</label>
-                                    <input type="text" name="text" id="text" onChange={e => setNameEmpresa(e.target.value)} required />
-                                    <label className="sr-only" htmlFor="nome_da_empresa">Nome da empresa</label>
-                                    {validateNameEmpresa ? null : <p><span>O nome da empresa precisa ter no mínimo 5 characters</span></p>}
-
-                                </div>
-                            </div>
-                        </div>
-
                         <div className="button">
                             <NavLink to="/createToken" className="LinkPropsButton register">
                                 <button onClick={handleSubmit} className={validateCamposButton ? "continueCorazul" : "continue"} disabled={!validateCamposButton} >
